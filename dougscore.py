76ac96eed49a3002e99b3a5bc5f2eed4cdfd3e63
analystbruh <chris.martin.data@gmail.com>
2021-07-02 09:25:09 -0400

    added dataset to repo

--- dougscore.py
+++ dougscore.py
@@ -5,4 +5,4 @@ url = 'https://docs.google.com/spreadsheets/u/0/d/e/2PACX-1vSYuGzOjstKqwnsG8TP2g
 df_scrape = scrape.scrape(url)
 df_cars = scrape.car_data(df_scrape)
 df_final = clean.clean_cols(df_cars, clean.key)
-df_final.to_csv('output/dougscore.csv')
+df_final.to_csv('dougscore.csv')
